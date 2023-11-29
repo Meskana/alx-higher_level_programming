9bb1c66490a531291a02919bd41bd83dffed546c
meskana <marcelinusilonuba21@gmail.com>
2023-11-29 15:24:26 -0800

    testint

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 
-for i in range(100):
-    if i == 99:
-        print("{}".format(i))
-    else:
-        print("{:02d}".format(i), end=', ')
+for i in range(0, 99):
+    print(f"{i:02}, ", end='')
