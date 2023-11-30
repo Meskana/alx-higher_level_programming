fbf77f19e8bdd76973b7e4f14660c6998169bef5
meskana <marcelinusilonuba21@gmail.com>
2023-11-30 12:40:07 -0800

    0-add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-
 import add_0
-    
-    a = 1
-    b = 2
 
-    print("{} + {} = {}".format(a, b, add_0.add(a, b))
+a = 1
+b = 2
+print("{} + {} = {}".format(a, b, add_0.add(a, b)))
 
 if __name__ == "__main__":
     pass
