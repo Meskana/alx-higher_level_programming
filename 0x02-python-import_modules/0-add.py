9f2bb2af6ddade4fe9d1e07c38116b217eac1b1a
meskana <marcelinusilonuba21@gmail.com>
2023-11-30 12:22:55 -0800

    0-add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-def add(a, b):
-    addition = a + b
-    print("{} + {} = {}".format(a, b, addition))
+
+import add_0
+    
+    a = 1
+    b = 2
+
+    print("{} + {} = {}".format(a, b, add_0.add(a, b))
+
 if __name__ == "__main__":
-    add(1,2)
+    pass
