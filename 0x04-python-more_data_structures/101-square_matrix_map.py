32b0cd62c29a41dabee32061944733dd52bf3608
meskana <marcelinusilonuba21@gmail.com>
2023-12-06 13:37:24 -0800

    0x04

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    new_mat = matrix.copy()
-    for i in range(len(matrix)):
-        new_mat[i] = list(map(lambda x: x ** 2, matrix[i]))
-    return new_mat
+def square_matrix_map(matrix=[]):
+    return (list(map(lambda x: list(map(lambda y: y ** 2, x)), matrix)))
