9039d5891056bd366fedb782f722f051e85db49c
meskana <marcelinusilonuba21@gmail.com>
2023-12-29 14:23:40 -0800

    pycode

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+
+"""
+an empty class Square that defines a Square
+"""
+
+
 class Square:
+
+    """
+    class that pass when called and accessed Attributes: No single attributes
+    """
     pass
