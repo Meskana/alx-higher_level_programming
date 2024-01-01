cbf87e97b7c11050b821868f4c52524a3802b5b6
meskana <marcelinusilonuba21@gmail.com>
2023-12-31 17:59:57 -0800

    add

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+A class Square that define a Square
+"""
+
+
+class Square:
+
+    """
+    Private instance attribute: size
+    """
+    def __init__(self, size_lenght):
+        self.__size = size_lenght
+
+
+Square.__module__ = '1-square'
