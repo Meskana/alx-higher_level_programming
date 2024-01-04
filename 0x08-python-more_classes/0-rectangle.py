2703d1173c3b290459f21893956587aafb1511d5
meskana <marcelinusilonuba21@gmail.com>
2024-01-04 09:45:54 -0800

    More Classes and Objects

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+ an empty class Rectangle that defines a rectangle
+"""
+
+
+class Rectangle:
+
+    """
+    class that pass when called and accessed Attributes: No single attributes
+    """
+
+    pass
