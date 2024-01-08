2baf922cdf9b5b746a090efa1c4a6b0e4650a8b3
meskana <marcelinusilonuba21@gmail.com>
2024-01-08 15:40:59 -0800

    task1

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+a function that returns the list of available attributes and
+methods of an object
+"""
+
+
+def lookup(obj):
+    """
+    Returns a list object
+    """
+
+
+return dir(list)
