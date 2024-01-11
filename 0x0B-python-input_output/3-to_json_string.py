779fd4107e5d4141a793bdb7a339b3f411202de5
meskana <marcelinusilonuba21@gmail.com>
2024-01-11 12:48:18 -0800

    task3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import json
+"""
+a function that returns the JSON representation of an object (string)
+"""
+
+
+def to_json_string(my_obj):
+    """
+    Converts the given Python object to its JSON representation as a string.
+    Parameters:
+        my_obj: The Python object to be converted.
+    Returns:
+        str: The JSON representation of the object as a string.
+    """
+
+    json_string = json.dumps(my_obj)
+    return json_string
