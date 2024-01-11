312974805a6b6c5f48944b1b099347c37ef5af37
meskana <marcelinusilonuba21@gmail.com>
2024-01-10 16:18:32 -0800

    pystle

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
 
 """
-A function that appends a string to a text file and returns the number of characters appended.
+A function that appends a string to a text file
+and returns the number of characters appended.
 
 Parameters:
     filename (str): The name of the file to which the text will be appended.
     text (str): The string to be appended to the file.
-
 Returns:
     int: The number of characters appended to the file.
 """
 
-def append_write(filename="", text=""):
 
+def append_write(filename="", text=""):
 
     """
     Appends the given text to the specified file.
-
     Args:
-        filename (str): The name of the file to which the text will be appended.
+    filename (str): The name of the file to which the
+        text will be appended.
         text (str): The string to be appended to the file.
-
     Returns:
         int: The number of characters appended to the file.
     """
