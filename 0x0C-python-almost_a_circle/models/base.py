6d1e41c979f09939d803aef295e52672ba786ebe
meskana <marcelinusilonuba21@gmail.com>
2024-01-16 15:48:53 -0800

    test

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
 """
-class: this is a Base class
+Class representing a base object.
+
+Attributes:
+    __nb_objects (int): Class variable to track the last assigned id.
+    id (int): The identifier assigned to an instance of the Base class.
 """
-import unittest
 
 
-class Base(unittest.TestCase):
+class Base:
     __nb_objects = 0
     """
-    Parameters:
-    - id (int): The name of the.
+    nitialize a new instance of the Base class.
+    Args:
+        id (int, optional): The identifier for the object.
+        If not provided, the next available id will be assigned.
     """
-
     def __init__(self, id=None):
         if id is not None:
             self.id = id
         else:
-            Base.__nb_objects +=1
+            Base.__nb_objects += 1
             self.id = Base.__nb_objects
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -10,13 +10,25 @@ class Test_base(unittest.TestCase):
         self.a = Base()
         self.b = Base()
         self.c = Base()
-        self.d = Base(12)
+        self.d = Base()
         self.e = Base()
-    
+   
+    """
+    testing the Equals valu.
+    """
     def test_isEquals(self):
         self.assertEqual(self.a.id,1)
         self.assertEqual(self.b.id,2)
         self.assertEqual(self.c.id,3)
-        self.assertEqual(self.d.id,12)
-        self.assertEqual(self.e.id,4)
-
+        self.assertEqual(self.d.id,4)
+        self.assertEqual(self.e.id,5)
+        
+    """
+    testing the integer value.
+    """
+    def test_assertIsInstance(self):
+        self.assertIsInstance(self.a,Base)
+        self.assertIsInstance(self.b,Base)
+        self.assertIsInstance(self.c,Base)
+        self.assertIsInstance(self.d,Base)
+        self.assertIsInstance(self.e,Base)        
