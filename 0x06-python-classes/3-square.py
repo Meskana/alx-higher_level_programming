32aebfc67e1ee06e0fcb59935427781c55dcfce4
meskana <marcelinusilonuba21@gmail.com>
2024-01-18 17:02:09 -0800

    task

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+a class Square that defines a square
+"""
+
+
+class Square:
+    """
+    method with parameter:
+    """
+    def __init__(self, size=0):
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    """ area of a square"""
+    def area(self):
+        return self.__size ** 2
+
+
+Square.__model__ = '3-Square'
