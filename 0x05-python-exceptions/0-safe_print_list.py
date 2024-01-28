2f24bb4d5b43e840d8f62640c1df854660825c72
meskana <marcelinusilonuba21@gmail.com>
2024-01-28 04:17:10 -0800

    try

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
+# a function that prints x elements of a list.
+
+
 def safe_print_list(my_list=[], x=0):
     count = 0
-    for i in range(x):
-        try:
-            print(my_list[i], end="")
+    try:
+        for i, e in enumerate(my_list):
+            if i >= x:
+                break
+            print(e, end="")
             count += 1
-        except:
-            break
+        print()
+    except TypeError as e:
+        print(e)
     return count
