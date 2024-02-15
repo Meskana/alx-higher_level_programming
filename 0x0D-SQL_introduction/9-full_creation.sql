b1c5e65569477f767fc883da099161af405936fd
meskana <marcelinusilonuba21@gmail.com>
2024-02-15 03:22:58 -0800

    sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,5 @@
 -- creates a table second_table in the database hbtn_0c_0
+
 CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, 'John', 10);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, 'Alex' 3);
