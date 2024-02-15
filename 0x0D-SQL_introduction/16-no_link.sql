5411c7ae14616333a18dca920278c31ccf703d01
meskana <marcelinusilonuba21@gmail.com>
2024-02-15 03:10:31 -0800

    sql

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,7 @@
+-- ists all records of the table second_table
+-- Don’t list rows without a name value
+-- Results should display the score and the name
+-- Records should be listed by descending score
+SELECT `score`, `name` FROM `second_table`
+WHERE `name` != ""
+ORDER BY `score` DESC
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- displays the number of records
+SELECT COUNT(*) FROM `first_table`
+WHERE `id` = 89;
