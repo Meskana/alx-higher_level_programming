94aeb92be8df3b41698152b69ad144549a6aa440
marcelinus <marcelinusilonuba21@gmail.com>
2024-03-11 18:09:06 -0700

    task 6 to 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+function factorial (n) {
+  if (isNaN(n) || n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+const arg = parseInt(process.argv[2]);
+console.log(factorial(arg));
---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const arg = process.argv[2];
+const intval = parseInt(arg);
+let i = 0;
+while (i < intval) {
+  console.log('c is fun');
+  i++;
+}
